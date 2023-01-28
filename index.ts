80ffe2ed283ddc213382f3be3244990ff8374c62
C. Hodor <120195957+Cosmin-Hodor@users.noreply.github.com>
2023-01-29 01:48:32 +0200

    Adding JSDoc comments for each function

--- index.ts
+++ index.ts
@@ -1,6 +1,16 @@
-const getChar = (position: number) => String.fromCharCode(Number(position) + 64);
+/**
+  @function
+  @param {number} position - The position of a character in the alphabet (1-based)
+  @returns {string} - The corresponding character in the alphabet
+*/
+const getChar = (position: number): string => String.fromCharCode(Number(position) + 64);
 
-const getId = (str: string) => {
+/**
+  @function
+  @param {string} str - The input string
+  @returns {string} - A unique identifier based on the input string
+*/
+const getId = (str: string): string => {
   let hash = 0;
   for (let i = 0; i < str.length; i++) {
     hash = str.charCodeAt(i) + ((hash << 5) - hash);
@@ -9,7 +19,11 @@ const getId = (str: string) => {
   return `${(hash.toString().substring(0, 5)).replace('-', '')}-${getChar(Math.abs(hash % 10))}`;
 };
 
-
+/**
+  Check if a class exists in the current document's head styles.
+  @param {any} className - The class name to check for.
+  @returns {(boolean | null)} Returns true if the class exists, null if it does not.
+*/
 const classExists = (className: any) => {
   const styleTags = Array.from(document.head.getElementsByTagName('style'));
 
@@ -27,10 +41,20 @@ const classExists = (className: any) => {
   return null;
 };
 
+/**
+  Converts a CSS object to a string.
+  @param {any} cssObject - The CSS object to be converted to a string.
+  @returns {string} - The resulting CSS string.
+*/
 const cssObjectToString = (cssObject: any) => {
   const cssStrings: any[] = [];
   const cssString = (JSON.stringify(cssObject) || '').toString();
 
+  /**
+    Recursive function that traverses the CSS object and builds the CSS string.
+    @param {any} obj - The current object being traversed.
+    @param {string} [selector='', newScope=false] - The current selector and whether a new scope is being created.
+  */
   function traverse(obj: any, selector = '', newScope = false) {
     const pointer = `${cssStrings?.length ? '}' : ''}.dom-${getId(cssString)} ${newScope ? selector : ''} { `;
     cssStrings.push(pointer);
@@ -57,6 +81,12 @@ type SortedCSSObject = {
   [key: string]: string,
 };
 
+/**
+  @function
+  @param {React.Component} Component - A React component that will be styled
+  @param {Function} styles - A function that takes in props and returns an object representing CSS styles
+  @returns {Function} - A higher-order component that applies the styles to the passed in component and adds a unique className
+*/
 const styled = (Component: any, styles: (props: any) => any) => {
   return (props: any) => {
     let sortedObject: SortedCSSObject = {};
